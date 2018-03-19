f7cd636384c4903f7af668d4b59c7f1b60e83a33
Stevenvr <vanroyesteven@gmail.com>
2018-03-19 22:52:33 +0100

    Solve TODOs Exercise 3

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -58,11 +58,22 @@ class Emojifier {
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
         }
 
-        // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+        // DONE (2): Iterate through the faces, calling getClassifications() for each face.
+        for (int i = 0; i < faces.size(); ++i) {
+            Face face = faces.valueAt(i);
+            getClassifications(face);
+        }
 
         // Release the detector
         detector.release();
     }
 
-    // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    // DONE (1): Create a static method called getClassifications() which logs the probability of
+    // each eye being open and that the person is smiling.
+
+    static void getClassifications(Face face){
+        Log.d(LOG_TAG, "Left eye: " + face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "Right eye: " + face.getIsRightEyeOpenProbability());
+        Log.d(LOG_TAG, "Smiley: " + face.getIsSmilingProbability());
+    }
 }
